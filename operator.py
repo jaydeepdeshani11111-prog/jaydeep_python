f1712bb79e0a76a43de8f583b59033e66e7235a6
jaydeepdeshani11111-prog <jaydeepdeshani11111@gmail.com>
2025-12-29 20:39:27 +0530

    Add files via upload
    operators

---
+++ operator.py
@@ -0,0 +1,38 @@
+#1. Assignment operator
+x=10
+print(x)
+#2. urnary minus
+a=5
+print(-a)
+#3. relational
+p=10
+q=5
+print (p>q)
+print (p==q)
+#4.logical
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+#5.boolean
+is_pass = True
+print(is_pass)
+#6.bitwise
+a=5
+b=3
+print(a & b)
+print(a | b)
+#7. membership
+numbers=[1,2,3,4,5,6]
+print(2 in numbers)
+print(5 not in numbers)
+#8.Identity
+x=10
+y=10
+print(x is y)
+print(x is not y)
+#9.Arithmetic 
+x=10
+y=10
+print(x+y)
