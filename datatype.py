d5861f33940dc11bb45635a2b96d6c40094f7ac9
jaydeepdeshani11111-prog <jaydeepdeshani11111@gmail.com>
2026-01-05 10:32:34 +0530

    Add files via upload
    datatype

---
+++ datatype.py
@@ -0,0 +1,30 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> x="Hello world"
+>>> print(x)
+Hello world
+>>> x="True"
+>>> print(x)
+True
+>>> x=20.5
+>>> print(x)
+20.5
+>>> x=1j
+>>> print(x)
+1j
+>>> x=["apple","banana","cheery"]
+>>> print(x)
+['apple', 'banana', 'cheery']
+>>> x=["apple","banana","cheery"]
+>>> print(x)
+['apple', 'banana', 'cheery']
+>>> x=none
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    x=none
+NameError: name 'none' is not defined. Did you mean: 'None'?
+>>> print(x)
+['apple', 'banana', 'cheery']
+>>> x={"name":"John","age":36}
+>>> print(x)
+{'name': 'John', 'age': 36}
