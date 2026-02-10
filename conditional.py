324fe9cd52d2a5ca2ea11fc70e54273323ff7932
jaydeepdeshani11111-prog <jaydeepdeshani11111@gmail.com>
2026-02-10 17:03:22 +0530

    Add files via upload
    if , else and elif condition statement example.

---
+++ conditional.py
@@ -0,0 +1,45 @@
+#1. Vote
+
+"""age = int(input("Enter your age: "))
+if age >=18:
+    print("Yes , you are eligible to vote")
+else:
+    print("No , you are not eligible to vote")"""
+
+#2. Marks
+
+"""marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("You will get A+.")
+elif marks >= 75:
+    print("You will get B+.")
+elif marks >= 35:
+    print("You will get C.")
+else:
+    print("Haha you are failed.")"""
+
+#3. Odd or Even
+
+"""value = int(input("Enter your value in decimal: "))
+if value %2 ==0:
+    print("Your number is Even number.")
+else:
+    print("Your value is Odd value.")"""
+
+#4. Positive or Negative
+
+num = int(input("Enter your Number: "))
+if num > 0:
+    print("your number is Positive.")
+elif num < 0:
+    print("your number is Negative.")
+else:
+    print("your number is Zero.")
+
+#5. License Eligiblity
+
+"""age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are ready to get your license.")
+else:
+    print("you can not ready to get your license.")"""
